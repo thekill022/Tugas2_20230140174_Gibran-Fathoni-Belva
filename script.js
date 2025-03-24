2e7ea9edbede5a508e84d74819d1a948b26a2cf2
thekill022 <gibfathoni@gmail.com>
2025-03-24 14:14:21 +0800

    menambahkan footer dan membuat javascript serta penerapan event dan DOM

---
+++ script.js
@@ -0,0 +1,11 @@
+let tanggal = new Date()
+
+document.querySelector('footer').innerHTML = '&copy;Copyright Gibran Fathoni Belva ' + tanggal.getFullYear() + " - All Right Reserve"
+
+function tampil() {
+    alert("Halo guys, aku button")
+}
+
+function show() {
+    alert("Halo Aku Meng, Aku kucing yang suka tidur dan males gerak, hehehe")
+}
